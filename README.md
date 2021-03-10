8786f626bfa6dbf449c531d474e28e1d8e26817e
SRRahmaaaan <sydurrahman2699@gmai.com>
2021-03-10 19:41:44 +0600

    finish responsive

---
+++ README.md
@@ -0,0 +1,1 @@
+my Project Name Is english premier league
--- src/components/TeamDetails/TeamDetails.js
+++ src/components/TeamDetails/TeamDetails.js
@@ -1,7 +1,8 @@
 import "./TeamDetails.css"
 import React, { useEffect, useState } from 'react'
-import { useParams } from "react-router-dom"
+import {useParams } from "react-router-dom"
 import Header from "../Header/Header"
+import {FaFacebook, FaInstagram, FaTwitter, FaYoutube} from "react-icons/fa"
 import Found from "./found.png"
 import Football from "./football.png"
 import Gender from "./gender.png"
@@ -16,12 +17,12 @@ const TeamDetails = () => {
         .then(response => response.json())
         .then(data => setTeamDetails(data.teams[0]))
     },[url])
-    const { strAlternate, intFormedYear, strCountry, strGender, strSport, strTeamBadge, strDescriptionEN,strStadium,
-                strStadiumDescription,strStadiumThumb, } = teamDetails;
-    console.log(teamDetails)
+    const {strAlternate, intFormedYear, strCountry, strGender, strSport, strTeamBadge, strDescriptionEN,strStadium,
+                strStadiumDescription,strStadiumThumb, strFacebook, strInstagram, strTwitter, strYoutube} = teamDetails;
     return (
         <section className = "team-details">
-            <Header><img src={strTeamBadge} className = "team-logo" alt="TeamBadge"/></Header>
+            <Header><img src={strTeamBadge} className="team-logo" alt="TeamBadge" /></Header>
+            
             <div className="general-info">
                 <div className="info">
                     <h2>{ strAlternate}</h2>
@@ -34,26 +35,31 @@ const TeamDetails = () => {
                     <h1>{ intFormedYear > 1900 ? "It's an nineteenth-century team" : "It's an eighteenth-century team"}</h1>
                 </div>
             </div>
+
             <div className="description">
                 <h3 className = "title">About The Club</h3>
                 <p>{ strDescriptionEN}</p>
             </div>
+
             <div className="stadium">
-                <div className="imgbx half-width">
+                <div className="imgbx">
                     <img src={strStadiumThumb} alt="Stadium" />
                 </div>
-                <div className="stadium-description half-width">
+                <div className="stadium-description">
                     <h3 className = "title">{strStadium }</h3>
                     <p>{ strStadiumDescription}</p>
                 </div>
             </div>
+
+            <div className="social-icon">
+                <a href={`https://${strFacebook}`} target = "_blank" rel = "noreferrer"><FaFacebook className = "sIcon" /></a>
+                <a href={`https://${strTwitter}`} target = "_blank" rel = "noreferrer"><FaTwitter className = "sIcon" /></a>
+                <a href={`https://${strInstagram}`} target = "_blank" rel = "noreferrer"><FaInstagram className = "sIcon" /></a>
+                <a href={`https://${strYoutube}`} target = "_blank" rel = "noreferrer"> <FaYoutube className = "sIcon" /></a>
+            </div>
+
         </section>
     )
 }
 
 export default TeamDetails
-    // strInstagram,
-    //   strTwitter,
-    //   strWebsite,
-    //   strYoutube,
-    //   strFacebook,
